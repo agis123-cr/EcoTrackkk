6014362ad28a561677fda66758a5b1973b528163
agis123-cr <gizzkyy@gmail.com>
2026-07-19 12:37:17 +0700

    Delete ECOTRACK directory

--- ECOTRACK/js/supabase.js
+++
@@ -1,8 +0,0 @@
-const supabaseUrl = "https://jmgxjnosnjoxjjmoybzt.supabase.co";
-const supabaseKey = "sb_publishable_gbad8zkeJB4ZCNloJ3Qa8w_02d-pbXj";
-
-
-const supabaseClient = supabase.createClient(
-  supabaseUrl,
-  supabaseKey
-);
